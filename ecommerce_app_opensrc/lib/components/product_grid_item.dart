8c5176c90fc17767a73520bf8c3ce784c9e21d53
AyushmanKS <ayushmanks1845@gmail.com>
2024-02-21 19:05:59 +0530

    initial commit

--- ecommerce_app_opensrc/lib/components/product_grid_item.dart
+++ ecommerce_app_opensrc/lib/components/product_grid_item.dart
@@ -20,18 +20,41 @@ class ProductGridItem extends StatelessWidget {
           ),
         );
       },
-      child: Card(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              product.name,
-            ),
-            Text(
-              product.description ?? "No description.",
-              textAlign: TextAlign.center,
-            ),
-          ],
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        clipBehavior: Clip.hardEdge,
+        child: Card(
+          elevation: 4.0,
+          child: Stack(
+            children: [
+              Image.asset(
+                product.image!.assetName,
+                fit: BoxFit.fill,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 16.0,
+                  ),
+                  child: Text(
+                    product.name,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
